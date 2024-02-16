25bf667278b1ea99ecc1dfa24709ff1c2b233253
raphazilla <--help>
2024-02-16 17:03:17 +0100

    New file

---
+++ mod4/debug_2.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+#
+# listausuarios.sh - Extrai usuarios do /etc/passwd
+#
+# Site:       https://www.exemplo.com
+# Autor:      Raphazilla
+# Manutencao: Raphazilla
+#
+# ------------------------------------------------------------------ #
+# Extrai usuarios do /etc/passwd, havendo a possibilidade de colocar
+# em maiusculo e em ordem alfabetica.
+#
+# Exemplos:
+#   $ ./listausuarios.sh -s -m
+#   Nexte examplo os script ira ordenar e colocar em maiusculo.
+# ------------------------------------------------------------------ #
+# Historico:
+#       v1.0 15/02/2024, Rapha:
+#           - Inicio do Programa
+#       v1.1 15/02/2024, Rapha:
+#           - Mudando if para case
+#       v1.2 15/02/2024, Rapha:
+#           - Flags adicionadas
+# ------------------------------------------------------------------ #
+# Testado em:
+#   bash 5.1.16(1)
+# ------------------------------------------------------------------ #
+
+# ----------------------------VARIAVEIS----------------------------- #
+
+# -----------------------------TESTES------------------------------- #
+
+# -----------------------------FUNCOES------------------------------ #
+
+# ----------------------------EXECUCAO------------------------------ #
+
+# ------------------------------------------------------------------ #
